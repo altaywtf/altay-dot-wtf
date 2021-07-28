aa07082f3ab86ba45c460274f966200d27060ebb
Altay Aydemir <altay@aydemir.io>
2021-07-28 20:09:50 +0200

    update md links to be ismilar to reeder app

--- src/components/Markdown/MDLink.tsx
+++ src/components/Markdown/MDLink.tsx
@@ -1,17 +1,29 @@
 import NextLink from 'next/link'
-import { Link } from 'rebass'
+import { Link, SxStyleProp } from 'rebass'
 
 type Props = { href: string }
+
+const style: SxStyleProp = {
+  color: 'linkPrimary',
+  paddingX: '0.1rem',
+  marginX: '-0.1rem',
+  borderRadius: 4,
+  '&:hover': {
+    backgroundColor: 'linkPrimary',
+    color: 'background',
+  },
+}
 
 const MDInternalLink: React.FC<Props> = ({ href, children }) => (
   <NextLink href={href} passHref>
     <Link
       href={href}
       sx={{
+        ...style,
         '&.scrolled-source-link': {
           backgroundColor: 'linkPrimary',
           color: 'background',
-          padding: 1,
+          paddingX: 1,
           borderRadius: 4,
         },
         '&.scrolled-source-link:hover': {
@@ -25,7 +37,7 @@ const MDInternalLink: React.FC<Props> = ({ href, children }) => (
 )
 
 const MDExternalLink: React.FC<Props> = ({ href, children }) => (
-  <Link href={href} target="_blank" rel="noreferrer noopener">
+  <Link href={href} target="_blank" rel="noreferrer noopener" sx={style}>
     {children}
   </Link>
 )
