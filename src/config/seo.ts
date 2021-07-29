023253e9839854340e0af2ffa3b4dabb62836a5a
Altay Aydemir <altay@aydemir.io>
2021-07-29 11:55:31 +0200

    update meta image generation

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,12 +1,5 @@
 import { NextSeoProps } from 'next-seo'
-import {
-  SITE_TITLE,
-  SITE_DESCRIPTION,
-  SITE_URL,
-  DEFAULT_IMETA_IMAGE_PATH,
-  META_IMAGE_HEIGHT,
-  META_IMAGE_WIDTH,
-} from './meta'
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from './meta'
 
 export const SEO: NextSeoProps = {
   title: SITE_TITLE,
@@ -20,13 +13,5 @@ export const SEO: NextSeoProps = {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     url: SITE_URL,
-    images: [
-      {
-        url: SITE_URL + DEFAULT_IMETA_IMAGE_PATH,
-        width: META_IMAGE_WIDTH,
-        height: META_IMAGE_HEIGHT,
-        alt: SITE_TITLE,
-      },
-    ],
   },
 } as const
--- src/core/api/openGraph.ts
+++ src/core/api/openGraph.ts
@@ -1,5 +1,4 @@
-import { META_IMAGE_HEIGHT, META_IMAGE_WIDTH } from 'config'
-import { OpenGraphImages } from 'next-seo/lib/types'
+import type { OpenGraphImages } from 'next-seo/lib/types'
 
 const OG_IMAGE_SERVICE_BASE_URL = `https://og-image.altay.wtf/api`
 
@@ -25,18 +24,15 @@ type Page = {
 type OpenGraphImageQueryParams = Note | Book | Page
 
 export const getOpenGraphImage = (params: OpenGraphImageQueryParams): OpenGraphImages => {
-  const searchParams = new URLSearchParams()
-
-  Object.entries(params).forEach(([key, value]) => {
-    searchParams.append(key, value)
-  })
+  const searchParams = Object.entries(params).reduce((acc, [key, value]) => {
+    acc.append(key, value)
+    return acc
+  }, new URLSearchParams())
 
   const url = new URL(OG_IMAGE_SERVICE_BASE_URL) + `?${searchParams}`
 
   return {
     alt: params?.title || '',
-    width: META_IMAGE_WIDTH,
-    height: META_IMAGE_HEIGHT,
     url,
   }
 }
