2011613c8f95b2810fa830df9fa21db61d108db1
Altay Aydemir <altay@aydemir.io>
2021-08-16 11:38:35 +0200

    fix redirections

--- next.config.js
+++ next.config.js
@@ -27,6 +27,36 @@ module.exports = withBundleAnalyzer({
         permanent: true,
       },
       {
+        source: '/notes',
+        destination: '/posts',
+        permanent: true,
+      },
+      {
+        source: '/notes/:path*',
+        destination: '/posts/:path*',
+        permanent: true,
+      },
+      {
+        source: '/images/notes/:path*',
+        destination: '/images/posts/:path*',
+        permanent: true,
+      },
+      {
+        source: '/blog',
+        destination: '/posts',
+        permanent: true,
+      },
+      {
+        source: '/blog/:path*',
+        destination: '/posts/:path*',
+        permanent: true,
+      },
+      {
+        source: '/images/blog/:path*',
+        destination: '/images/posts/:path*',
+        permanent: true,
+      },
+      {
         source: '/analytics',
         destination: '/analytics.html',
         permanent: true,
