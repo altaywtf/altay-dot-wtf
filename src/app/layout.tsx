c50df495fe7176c1e7b9d7a8c94b3010f88c328e
Altay Aydemir <altay@aydemir.io>
2022-11-07 12:33:10 +0300

    update .gitignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import '../theme/style.css'
-import RootStyleRegistry from './emotion'
 import AnalyticsWrapper from 'components/AnalyticsWrapper'
 import ThemeProvider from 'components/ThemeProvider'
+import '../theme/style.css'
+import RootStyleRegistry from './emotion'
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en">
