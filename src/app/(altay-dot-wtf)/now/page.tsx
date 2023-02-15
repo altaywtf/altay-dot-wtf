44d26a50d93f39898e9a0ea9616f7490291b00e2
Altay Aydemir <altay@aydemir.io>
2023-02-15 18:02:51 +0300

    update now/revalidation

--- src/app/(altay-dot-wtf)/now/page.tsx
+++ src/app/(altay-dot-wtf)/now/page.tsx
@@ -2,7 +2,11 @@ import { API_URL } from 'config'
 import NowPage from './NowPage'
 
 const fetchData = async () => {
-  const res = await fetch(`${API_URL}/now`)
+  const res = await fetch(`${API_URL}/now`, {
+    next: {
+      revalidate: 60 * 60 * 1000 * 24, // 24 hours
+    },
+  })
   const { now } = await res.json()
   return now
 }
