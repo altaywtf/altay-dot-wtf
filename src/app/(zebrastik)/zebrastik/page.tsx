65f458515581760af16b18b0140f0cb5eac05d6f
Altay <9790196+altaywtf@users.noreply.github.com>
2023-05-07 22:29:04 +0300

    update zebrastik

--- src/app/(zebrastik)/zebrastik/page.tsx
+++ src/app/(zebrastik)/zebrastik/page.tsx
@@ -3,20 +3,20 @@ import { zebrastikCopy } from 'config'
 
 const Zebrastik = () => (
   <>
-    <div className="relative h-28 w-28 overflow-hidden rounded-lg">
+    <div className="relative h-28 w-28 overflow-hidden rounded">
       <Image src="/images/zebra.jpg" alt="zebrastik logo" fill />
     </div>
 
     <div className="mt-4 flex flex-col gap-2">
       <h1>{zebrastikCopy.title}</h1>
-      <p className="text-gray-400">{zebrastikCopy.description}</p>
+      <p className="text-neutral-400">{zebrastikCopy.description}</p>
     </div>
 
     <hr className="my-8" />
 
     <a href="mailto:altay@zebrastik.com">altay@zebrastik.com</a>
 
-    <div className="mt-4 text-gray-400">
+    <div className="mt-4 text-neutral-400">
       <p>zebrastik, LLC</p>
       <p>30 N Gould St, STE 4000</p>
       <p>Sheridan, WY 82801</p>
