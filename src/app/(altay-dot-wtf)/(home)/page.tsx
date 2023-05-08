b293adbc80ee1ca06f6cfa0e1ed371baaa646c7a
Altay <9790196+altaywtf@users.noreply.github.com>
2023-05-08 11:15:02 +0300

    fix header and home styling on mobile

--- src/app/(altay-dot-wtf)/(home)/page.tsx
+++ src/app/(altay-dot-wtf)/(home)/page.tsx
@@ -38,7 +38,7 @@ const HomePage = async () => {
     <Page header={{ title: homeCopy.title }}>
       <Markdown>{data.description}</Markdown>
 
-      <div className="mt-3 flex flex-row gap-3">
+      <div className="mt-3 flex flex-row flex-wrap gap-3">
         {data.contactLinks.map(({ title, url }) => (
           <a
             key={title}
