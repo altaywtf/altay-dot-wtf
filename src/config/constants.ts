1f7ee92953428ca10e523e4f50ed10d3c30e4bd4
Altay <altay@zebrastik.com>
2023-11-30 22:26:27 +0300

    chore: update APP_URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const APP_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:1994'
-    : process.env.VERCEL_URL
+    : `https://${process.env.VERCEL_URL || 'altay.wtf'}`
 
 export const API_URL = `${APP_URL}/api`
