79c076f315f6076b08f90f62d92b56647a46d3f5
Altay <altay@hey.com>
2024-09-16 13:12:05 +0300

    chore: update avatar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -68,8 +68,13 @@ const COLLECTIONS: Array<Omit<Project, 'icon_url'>> = [
 
 const HomePage = () => (
   <>
-    <div className="relative h-20 w-20 overflow-hidden rounded border border-solid border-neutral-900">
-      <Image alt="That's my head" fill sizes="100%" src="/images/avatar.png" />
+    <div className="relative h-24 w-24 overflow-hidden rounded border border-solid border-neutral-900">
+      <Image
+        alt="That's my head"
+        fill
+        sizes="100%"
+        src="/images/2024-aa-sq.png"
+      />
     </div>
 
     <div className="mt-8 flex flex-col gap-4">
